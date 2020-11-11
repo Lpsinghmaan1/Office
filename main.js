d13306849b27ef0a455b401a592591060103c866
Lovepreet Singh Maan <lsinghmaan@console.com.au>
2020-11-12 08:33:09 +1000

    12/11

--- main.js
+++ main.js
@@ -3,3 +3,24 @@ let myRegex = /<.*?>/; // Change this line
 let result = text.match(myRegex);
 
 console.log(result)
+
+class People {
+constructor (name,area,age)
+ {
+     this.name = name;
+     this.area = area;
+     this.age = age;
+}
+}
+
+const lp = new.People("lp","support",28)
+
+const myobject = {
+    name : 'lp',
+    Age : '28',
+    human : true
+}
+
+myobject.origin = 'india';
+
+console.log(myobject);
